89d58f5b6b5b716871799107adbf315ffdb9ef11
sangwoo89118 <sangwoo89118@gmail.com>
2018-01-30 13:48:45 -0800

    commit before change files

---
+++ server/php_mailer/mail_handler.php
@@ -0,0 +1,103 @@
+<?php
+require_once('email_config.php');
+require('phpmailer/PHPMailer/PHPMailerAutoload.php');
+
+
+
+$message=[];
+$output=[
+    'success' => null,
+    'messages' => []
+];
+
+//Sanitize name field
+$message['name'] = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+if(empty($message['name'])){
+    $output['success'] = false;
+    $output['messages'][] = 'missing name key';
+}
+$message['company'] = filter_var($_POST['company'], FILTER_SANITIZE_STRING);
+if(empty($message['name'])){
+    $output['success'] = false;
+    $output['messages'][] = 'missing name key';
+}
+//Validate email field
+$message['email'] = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+if(empty($message['email'])){
+    $output['success'] = false;
+    $output['messages'][] = 'invalid email key';
+}
+
+$message['phone'] = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
+if(empty($message['name'])){
+    $output['success'] = false;
+    $output['messages'][] = 'missing name key';
+}
+//Sanitize message
+$message['message'] = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+if(empty($message['message'])){
+    $output['success'] = false;
+    $output['messages'][] = 'missing message key';
+}
+
+
+
+if ($output['success'] !== null) {
+    http_response_code(400);
+    echo json_encode($output);
+    exit();
+}
+
+
+$mail = new PHPMailer;
+// $mail->SMTPDebug = 3;           // Enable verbose debug output. Change to 0 to disable debugging output.
+
+$mail->isSMTP();                // Set mailer to use SMTP.
+$mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers.
+$mail->SMTPAuth = true;         // Enable SMTP authentication
+
+
+$mail->Username = EMAIL_USER;   // SMTP username
+$mail->Password = EMAIL_PASS;   // SMTP password
+$mail->SMTPSecure = 'tls';      // Enable TLS encryption, `ssl` also accepted, but TLS is a newer more-secure encryption
+$mail->Port = 587;              // TCP port to connect to
+$options = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+$mail->smtpConnect($options);
+$mail->From = $_POST['email'];  // sender's email address (shows in "From" field)
+$mail->FromName = $_POST['email'];   // sender's name (shows in "From" field)
+$mail->addAddress('sangwoo89118@gmail.com', 'First Recipient');  // Add a recipient
+//$mail->addAddress('ellen@example.com');                        // Name is optional
+$mail->addReplyTo($_POST['email']);                          // Add a reply-to address
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject'; //IP addresss
+$mail->Body    = $message['message']; //whatever body that they sent me
+$mail->AltBody = htmlentities($message['message']); //
+$output = [
+  'success'=> true,
+
+];
+
+//need to JSON values
+if(!$mail->send()) { // -> php object oriented. period of javascript
+    $output['success'] = false;
+    $output['message']= $mail->ErrorInfo;
+
+} else {
+    // echo 'Message has been sent';
+    $output['success'] = true;
+}
+
+print (json_encode($output));
+?>
